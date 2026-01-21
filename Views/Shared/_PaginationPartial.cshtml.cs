f83ef6687afd74f92c9ace06179542255e93d504
mcfm25 <m.carla@fm-technology.it>
2026-01-21 12:36:30 +0100

    Add files via upload

---
+++ Views/Shared/_PaginationPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Trasformazioni.Views.Shared
+{
+    public class _PaginationPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
